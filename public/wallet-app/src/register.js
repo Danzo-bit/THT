857f7e29d3cf32ecc858089cc88e799095291fea
danielElcorp <dannydix555@gmail.com>
2023-03-10 05:13:14 +0100

    modifies Register component

--- public/wallet-app/src/register.js
+++ public/wallet-app/src/register.js
@@ -4,12 +4,14 @@ import axios from 'axios';
 function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
   const [error, setError] = useState('');
 
   async function handleSubmit(event) {
     event.preventDefault();
     try {
-      const response = await axios.post('/api/v1/auth/register', { email, password });
+      const response = await axios.post('/api/v1/auth/register', {firstname,lastname, email, password });
       // Do something with the newly created user, e.g. store the user data in state or cookies
       console.log(response.data.user);
     } catch (error) {
@@ -21,6 +23,16 @@ function Register() {
     <div>
       <h2>Register</h2>
       <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input type="text" value={firstname} onChange={(event) => setFirstname(event.target.value)} required />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input type="text" value={lastname} onChange={(event) => setLastname(event.target.value)} required />
+        </label>
+        <br />
         <label>
           Email:
           <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} required />
